68b01a5262779db5feaa9e1b21d9ab53bd471468
Waruni <waruniwickramanayake@gmail.com>
2015-06-22 16:00:07 +1000

    Bootstrap file

--- solvup-vagrant/bootstrap.sh
+++ solvup-vagrant/bootstrap.sh
@@ -24,21 +24,49 @@ BOTO_CONF
 
     apt-get -q -y install puppet git
 
-    wget --quiet https://github.com/wwaruni/puppet/blob/master/solvup.dev.zip -O solvup.zip
+    wget --quiet https://github.com/wwaruni/puppet/raw/master/solvup.dev.zip -O solvup.zip
 
-    unzip solvup.zip
+    sudo apt-get install unzip
+    unzip  solvup.zip
     rsync -rCcvz solvup.dev/ /etc/puppet/
     puppet apply --verbose --debug /etc/puppet/manifest/init.pp
 
-    if [! -d "/var/www/solvup.com" ];
-    then
-        mkdir -p /var/www/solvup.com
-    fi
+    sudo mkdir  /var/www/solvup_tic
+    sudo mkdir  -p /var/www/solvup_tic/RIA_RepairsTicGroup
 
-    git clone https://gexchai:Altona3025@github.com/Conduct/RIA_RepairsTicGroup.git
+    curl -LOk  https://gexchai:Altona3025@github.com/Conduct/RIA_RepairsTicGroup/archive/release-2.52.zip
 
-    mv RIA_RepairsTicGroup/* /var/www/solvup.com/
+    unzip release-2.52.zip 
+
+
+    sudo mv -f RIA_RepairsTicGroup-release-2.52/* /var/www/solvup_tic/RIA_RepairsTicGroup/
+    sudo cp -f /var/www/solvup_tic/RIA_RepairsTicGroup/app/config/bootstrap_LIVE.php  /var/www/solvup_tic/RIA_RepairsTicGroup/app/config/bootstrap.php
+    sudo cp -f /var/www/solvup_tic/RIA_RepairsTicGroup/app/config/core_LIVE.php /var/www/solvup_tic/RIA_RepairsTicGroup/app/config/core.php
+    sudo cp -f /var/www/solvup_tic/RIA_RepairsTicGroup/app/config/database_LIVE.php /var/www/solvup_tic/RIA_RepairsTicGroup/app/config/database.php
+
+    sudo mkdir -p /var/www/solvup_tic/RIA_RepairsTicGroup/app/tmp/cache
+    sudo mkdir -p /var/www/solvup_tic/RIA_RepairsTicGroup/app/tmp/cache/models
+    sudo mkdir -p /var/www/solvup_tic/RIA_RepairsTicGroup/app/tmp/cache/persistent
     
+    sudo chmod -R 777 /var/www/solvup_tic/RIA_RepairsTicGroup/app/tmp
+    sudo chown -R vagrant:vagrant /var/www/solvup_tic
+
+    sudo rm -rf /etc/php5/apache2/conf.d/20-mcrypt.ini
+
+    sudo ln -s   /etc/php5/mods-available/mcrypt.ini /etc/php5/apache2/conf.d/20-mcrypt.ini
+
+    sudo a2enmod ssl
+
+    sudo service apache2 restart
+
+
+    sudo mkdir -p /etc/apache2/ssl
+    sudo openssl req -batch -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/solvup.key -out /etc/apache2/ssl/solvup.crt
+
+    sudo a2ensite default-ssl.conf
+
+    sudo service apache2 restart
+
     touch /home/vagrant/.bootstrapped
 fi
 
